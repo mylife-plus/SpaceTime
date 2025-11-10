7df73cb41748f1ce991da999ffc896b2ff08c9a8
Unicorndev021 <unicorndev.02.1997@gmail.com>
2025-11-10 20:17:31 +0500

    Revert filter indicator to count location and radius as 2 separate filters
    Reverted the filter counting logic to treat location and radius as 2 distinct filters
    as per user requirement.
    
    Changes:
    - Location = 1 filter
    - Radius = 1 filter
    - Total when both applied = 2 filters
    
    This provides separate visibility for location and radius filters on the map view.

--- lib/app/modules/add_memories/views/mini_widgets/filter_indicator.dart
+++ lib/app/modules/add_memories/views/mini_widgets/filter_indicator.dart
@@ -34,15 +34,14 @@ class FilterIndicator extends StatelessWidget {
         }
       }
 
-      // Location and radius are treated as a single filter
       if (controller.selectedLocation.value.isNotEmpty) {
         activeFilterCount++;
-        // Show both location and radius info if radius is set
-        if (controller.selectedRadius.value.isNotEmpty) {
-          activeFilters.add('Location (${controller.selectedRadius.value})');
-        } else {
-          activeFilters.add('Location');
-        }
+        activeFilters.add('Location');
+      }
+
+      if (controller.selectedRadius.value.isNotEmpty) {
+        activeFilterCount++;
+        activeFilters.add('Radius');
       }
 
       if (controller.selectedHashtags.isNotEmpty) {
